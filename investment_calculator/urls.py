fa8e22e1017a65e4250eb2a2c060f31be9c6bc59
esta22 <noxcd123@naver.com>
2025-09-25 11:07:06 +0900

    Delete investment_calculator directory

--- investment_calculator/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from calculator.views import calculate_investment
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', calculate_investment, name='calculator'),
-    path('stock2/', include('stock2.urls')),  # stock2/ 아래에 포함
-]
